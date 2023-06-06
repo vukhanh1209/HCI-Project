2301ee0b32ac1f52f2c3e627a92c0306fb6d4525
vukhanh1209 <nguyenhongvukhanh@gmail.com>
2023-06-06 10:45:49 +0700

    update VerifyPage & scrolling

--- src/pages/VerifyPage/VerifyPage.tsx
+++ src/pages/VerifyPage/VerifyPage.tsx
@@ -121,7 +121,7 @@ const VerifyPage = () => {
           <div className='flex items-end gap-x-1'>
             <span className='text-lg font-semibold text-black dark:text-white md:text-2xl'>Chào mừng bạn đến</span>
             <span className='dynamic text-lg font-semibold text-red-600 after:bg-white dark:after:bg-[#0f0f0f] md:text-2xl'>
-              YouTube
+              HCITube
             </span>
           </div>
         </Link>
@@ -143,7 +143,8 @@ const VerifyPage = () => {
               classNameInput='rounded-lg border border-gray-400 py-2 px-3 placeholder:text-xs w-64 dark:bg-transparent text-black dark:text-white md:w-96 md:placeholder:text-sm outline-none text-xs md:text-sm'
             />
           </div>
-          <div className='flex items-center gap-x-1'>
+          <span className="text-xs font-semibold text-black dark:text-white md:text-sm">Nhập 6 ký tự bất kỳ để hoàn thành bước xác nhận</span>
+          {/* <div className='flex items-center gap-x-1'>
             <span className='text-xs text-black dark:text-white md:text-sm '>
               Mã này sẽ hết hạn sau &nbsp;
               {`${remainingTime.minutes.toString().padStart(2, '0')}:${remainingTime.seconds
@@ -158,7 +159,7 @@ const VerifyPage = () => {
             >
               Lấy mã mới
             </button>
-          </div>
+          </div> */}
           <Button
             className='mt-3 w-full rounded-lg bg-blue-600 p-2 text-xs font-semibold text-white md:text-sm'
             type='submit'
